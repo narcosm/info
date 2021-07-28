da2302f3d1184fda84041ee578f39f923e466f72
narcosm <narcosm@correo.udistrital.edu.co>
2021-07-27 23:09:55 -0500

    'app'

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const moment = require('moment');
+let os = require('os');
+
+
+let respuesta = () => {
+    let fecha = moment().format('MMMM Do YYYY, h:mm:ss a');
+    let texto = `Nombre host: ${os.hostname()}, Os: ${os.type()}, fecha: ${fecha}`;
+    return texto;
+}
+app.get('/', function (req, res) {
+    let mensaje = respuesta();
+    res.send(mensaje);
+});
+
+app.listen(3001, () => {
+    console.log('Sevidor arriba');
+});
